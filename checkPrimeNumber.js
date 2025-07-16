b92e12802adc069fa25f7b3e27ba1afb846fe49c
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-07-16 18:57:45 +0200

    this function return true if number is prime else false

---
+++ checkPrimeNumber.js
@@ -0,0 +1,14 @@
+// write function that returns true if given number is prime
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  const sqrt = Math.sqrt(num);
+  for (let i = 3; i <= sqrt; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+console.log(isPrime(4))
