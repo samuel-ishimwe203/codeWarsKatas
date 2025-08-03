522c657ee32318ee801e0ea89a950a7576cdeb5b
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-03 09:25:03 +0200

    updation

---
+++ Exercise.js
@@ -0,0 +1,9 @@
+
+
+// set is the bult-in javascript object that store unique value of an type , primitive and object refrences.
+
+const variable1 = new Set()
+variable1.add(1)
+variable1.add('hello')
+variable1.add({name : 'samuel'})
+variable1.add([1,3,4])
