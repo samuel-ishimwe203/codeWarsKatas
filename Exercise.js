78a231bb1fe96905218e618722724d204de17b3e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-03 10:10:03 +0200

    new changes

--- Exercise.js
+++ Exercise.js
@@ -7,3 +7,15 @@ variable1.add(1)
 variable1.add('hello')
 variable1.add({name : 'samuel'})
 variable1.add([1,3,4])
+
+console.log(variable1)
+
+
+const a = [1,2,3,4,5]
+const b = [3,5,6,7,8]
+
+const union = new Set([...a ,...b])
+const intersection= new Set([...a].filter(x=>b.has(x)))
+
+console.log(union)
+console.log(intersection)
