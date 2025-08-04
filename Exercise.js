2927830b660762da865f4a1f93deafe5952a06a5
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-04 08:51:01 +0200

    new updation

--- Exercise.js
+++ Exercise.js
@@ -11,11 +11,33 @@ variable1.add([1,3,4])
 console.log(variable1)
 
 
-const a = [1,2,3,4,5]
-const b = [3,5,6,7,8]
+// const a = [1,2,3,4,5]
+// const b = [3,5,6,7,8]
 
-const union = new Set([...a ,...b])
-const intersection= new Set([...a].filter(x=>b.has(x)))
+// const union = new Set([...a ,...b])
+// const intersection= new Set([...a].filter(x=>b.has(x)))
 
-console.log(union)
-console.log(intersection)
+// console.log(union)
+// console.log(intersection)
+
+// Return Negative even number is positive number
+
+
+function negativePostive(num){
+    if(num<0){
+        return num
+    }
+    return num*(-1)
+}
+
+console.log(negativePostive(5))
+
+// Square Every Digit
+
+function squareDigits(num){
+  
+  const squareDigits= num.toString().split('')
+  const result= squareDigits.map(e=>e*e).join('') 
+  
+  return Number(result)
+}
