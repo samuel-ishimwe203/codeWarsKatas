212c2cf1e5336c055f070200f7dddeacb830af9d
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-06 17:46:12 +0200

    new changes here

---
+++ my-tailwindcss/src/components/FirPart.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+function FirPart() {
+  return (
+    <div>
+            <div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Odit cum omnis accusantium reiciendis, consequuntur adipisci,
+                    impedit quibusdam assumenda itaque odio rerum, magnam tempore? 
+                    Sequi nulla impedit sunt omnis nisi quisquam.
+                </div>
+                <div>
+                     <img src="" alt="" />
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default FirPart
