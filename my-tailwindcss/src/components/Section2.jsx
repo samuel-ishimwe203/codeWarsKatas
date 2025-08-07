3b7b167fd942912d2898f27f58d2d4f85e66ee17
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-07 12:27:55 +0200

    new changes

--- my-tailwindcss/src/components/Section2.jsx
+++ my-tailwindcss/src/components/Section2.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
  function Section2() {
   return (
-    <div>
-         <nav>
-              <a href="#">Drivers</a>
-              <a href="#"> All Drivers</a>
-              <a href="#">GetForce Drivers</a>
-              <a href="#">Networking Drivers</a>
+    <div  className='flex  bg-black p-4'>
+         <nav className=' flex text-white gap-10  px-15'>
+                
+              <a className='text-white font-bold text-2xl ' href="#">Drivers</a>
+              <a className=' hover:text-green-500 mt-2' href="#"> All Drivers</a>
+              <a className=' hover:text-green-500 mt-2' href="#">GetForce Drivers</a>
+              <a className=' hover:text-green-500 mt-2' href="#">Networking Drivers</a>
          </nav>
     </div>
   )
 }
+export default Section2
---
+++ my-tailwindcss/src/components/Section3.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Section3() {
+  return (
+    <div>
+            <div>
+                <p>Get Automatic Driver Updates</p>
+                <p>The NVIDIA App is the essential companion for PC 
+                   gamers and creators. Keep your PC up to date with 
+                   the latest NVIDIA drivers and technology.</p>
+            </div>
+            <div>
+                <p>Best for:</p>
+                <p>Gamers / Creators</p>
+                <p>Best for:</p>
+                <p>Professionals / WorkerStation Users</p>
+            </div>
+    </div>
+  )
+}
+
+export default Section3
