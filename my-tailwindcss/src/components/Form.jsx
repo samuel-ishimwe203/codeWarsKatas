efc8e84cfeecedf645be5d5a852cc1478b34ad3f
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-07 13:58:19 +0200

    new changes

--- my-tailwindcss/src/components/Form.jsx
+++ my-tailwindcss/src/components/Form.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Form() {
   return (
-    <div className='m-10'>
+    <div className='m-10 '>
           <div><p className=' text-black font-black text-2xl'>Manual Driver Search</p></div>
-          <div className='mt-3 '><input type="text" className='border-none outline-none' placeholder='search for product product type or series' /></div>
+          <div className='mt-3 grid grid-cols-2 '> <div><input type="text" className='border-none outline-none w-[65%]' placeholder='search for product product type or series' /></div>
+          <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg></div>
+</div>
+
+    <div>
+        <select>
+               <option className=' border border-solid border-lime-600 p-8' value="">Select property categories</option>
+        </select>
+   </div>
+
     </div>
   )
 }
