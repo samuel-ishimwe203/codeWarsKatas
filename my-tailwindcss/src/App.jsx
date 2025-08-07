4055ff06f41a263b3af0d27860c4774242f73d48
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-07 09:33:11 +0200

    new changes

--- my-tailwindcss/src/App.jsx
+++ my-tailwindcss/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-import Header from './components/Header'
+
+import Header1 from './components/Header1.jsx'
+import Section2 from './components/Section2.jsx'
 
 function App() {
   
 
   return (
     <>
-        <Header/>
+      <Header1/>
+
+      <Section2/>
     </>
   )
 }
---
+++ my-tailwindcss/src/components/Section2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ function Section2() {
+  return (
+    <div>
+         <nav>
+              <a href="#">Drivers</a>
+              <a href="#"> All Drivers</a>
+              <a href="#">GetForce Drivers</a>
+              <a href="#">Networking Drivers</a>
+         </nav>
+    </div>
+  )
+}
