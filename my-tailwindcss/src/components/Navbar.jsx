9a021ace5fde209e46afc07fc57d86278407e1f8
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-12 17:58:01 +0200

    new changes

--- my-tailwindcss/src/components/Navbar.jsx
+++ my-tailwindcss/src/components/Navbar.jsx
@@ -3,18 +3,29 @@ import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 
 function Navbar() {
   return (
-    <div className='bg-black flex px-4 justify-between items-center h-24 max-w-[1240px] mx-auto  text-white '>
+    <div className='bg-black flex px-4 justify-between items-center h-full max-w-[1240px] mx-auto  text-white '>
         <h1 className='w-full text-[#00df9a] font-bold text-2  xl'>REACT.</h1>
 
-         <ul className='flex'>
+         <ul className='flex hidden '>
             <li className='p-4'>Home</li>
             <li className='p-4'>Company</li>
             <li className='p-4'>Resources </li>
             <li className='p-4'>About</li>
             <li className='p-4'>contact</li>
         </ul>
+
         <div>
-            <AiOutlineMenu/>
+            <AiOutlineMenu size={20}/>
+        </div>
+
+        <div className='fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900'>
+             <ul className='pt-10' >
+                 <li className='p-4'>Home</li>
+                 <li className='p-4'>Company</li>
+                 <li className='p-4'>Resources </li>
+                 <li className='p-4'>About</li>
+                 <li className='p-4'>contact</li>
+             </ul>
         </div>
     </div>
   )
