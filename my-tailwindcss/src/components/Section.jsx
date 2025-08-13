065b91d9cbed2174eff2735c7526e7a792bdab78
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-13 08:49:23 +0200

    new changes

---
+++ my-tailwindcss/src/components/Section.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+function Section() {
+  return (
+    <div className='text-white'>
+
+        <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+                    
+                <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>    
+                <h1 className='md:text-7xl sm:text-5xl text-4xl font-bold md:py-6 '>Grow with data.  </h1>
+              <div>
+                   <p>Fast , flexible financing for</p>
+              </div>
+       </div>
+
+    </div>
+  )
+}
+
+export default Section 
