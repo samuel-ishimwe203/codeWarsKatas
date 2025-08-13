2a8a6381a9f0c62f338f5989cfdbec71905740a3
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-13 07:59:01 +0200

    new changes

--- my-tailwindcss/src/components/Navbar.jsx
+++ my-tailwindcss/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import {useState} from 'react'
 import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai' 
 
 function Navbar() {
+  const [nav , Setnav]=useState(false)
+
+  const handleNav =
+
   return (
     <div className='bg-black flex px-4 justify-between items-center h-full max-w-[1240px] mx-auto  text-white '>
         <h1 className='w-full text-[#00df9a] font-bold text-2  xl'>REACT.</h1>
@@ -18,14 +22,14 @@ function Navbar() {
             <AiOutlineMenu size={20}/>
         </div>
 
-        <div className='fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900'>
-             <h1 className='w-full text-[#00df9a] font-bold text-2xl m-4'>REACT.</h1>
-             <ul className='pt-10 uppercase' >
-                 <li className='p-4'>Home</li>
-                 <li className='p-4'>Company</li>
-                 <li className='p-4'>Resources </li>
-                 <li className='p-4'>About</li>
-                 <li className='p-4'>contact</li>
+        <div className='fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-black'>
+             <h1 className='w-full text-[#00df9a] font-bold text-xl p-4'>REACT.</h1>
+             <ul className='p-2 uppercase' >
+                 <li className='p-4 border-b border-gray-600'>Home</li>
+                 <li className='p-4 border-b border-gray-600'>Company</li>
+                 <li className='p-4 border-b border-gray-600'>Resources </li>
+                 <li className='p-4 border-b border-gray-600'>About</li>
+                 <li className='p-4 border-b border-gray-600'>contact</li>
              </ul>
         </div>
     </div>
